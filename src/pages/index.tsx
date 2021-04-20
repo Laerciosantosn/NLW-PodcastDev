e419b821aa97c925dd86b3c7eaa931b6e6c2ffbb
Laercio Santos <laerciosantosn@gmail.com>
2021-04-20 17:18:33 -0300

    Created the server with Jason-server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+// SPA useEffect e um exemplo de SPA
+// SSR getServerSideProps ou getStaticProps o next ja entende que deve executar a função antes de exibir a pagina
+// SSG So funciona em produção
 
 export default function Home() {
+
   return (
     <>
       <h1>Index</h1>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 *60 * 8,
+  }
 }
